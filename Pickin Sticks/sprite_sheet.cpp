f1df7546d395a7a0f665466ef1f49dc439a402df
Kayne Ruse <ratstail91@yahoo.com.au>
2013-01-03 01:54:04 +1100

    Incomplete, committing anyway, see details
    This was incomplete when I wrote it, and since I'm not working on it at
    the moment, I'm committing the incomplete version. I don't have time right
    now to go through it, but I should still push it, since it's a bad idea to
    have something uncommitted sitting on my drive for so long.

--- Pickin Sticks/sprite_sheet.cpp
+++ Pickin Sticks/sprite_sheet.cpp
@@ -1,2 +1,108 @@
 #include "sprite_sheet.h"
 
+//-------------------------
+/* Public access members */
+//-------------------------
+
+SpriteSheet::SpriteSheet() {
+	//
+}
+
+SpriteSheet::~SpriteSheet() {
+	//
+}
+
+void SpriteSheet::Update() {
+	animator.Update();
+}
+
+void SpriteSheet::LoadSurface(const char* fname, Uint16 width, Uint16 height) {
+	//This was ripped from Actor
+	Image::LoadSurface(fname);
+
+	SetWidth(width);
+	SetHeight(height);
+}
+
+void SpriteSheet::SetSurface(SDL_Surface* surface, Uint16 width, Uint16 height) {
+	//This was ripped from Actor
+	Image::SetSurface(surface);
+
+	SetWidth(width);
+	SetHeight(height);
+}
+
+void SpriteSheet::DrawTo(SDL_Surface* const dest, Sint16 x, Sint16 y) {
+	//This was ripped from Actor
+	//set the current frame
+	Image::SetClipX(animator.GetFrame() * Image::GetClipW());
+
+	//draw to the correct pos, and hook the camera
+	Image::DrawTo(dest, x, y);
+}
+
+Uint16 SpriteSheet::SetWidth(Uint16 w) {
+	//This was ripped from Actor
+	//set the values
+	Image::SetClipW( w ? w : Image::GetSurface()->w );
+
+	//adjust the spritesheet
+	animator.SetFrameCount( w ? Image::GetSurface()->w / w : 0 );
+
+	return Image::GetClipW();
+}
+
+Uint16 SpriteSheet::SetHeight(Uint16 h) {
+	//This was ripped from Actor
+	//set the values
+	Image::SetClipH( h ? h : Image::GetSurface()->h );
+
+	//reset the sprite index (lazy)
+	Image::SetClipY( 0 );
+
+	return Image::GetClipH();
+}
+
+Uint16 SpriteSheet::GetWidth() {
+	return Image::GetClipW();
+}
+
+Uint16 SpriteSheet::GetHeight() {
+	return Image::GetClipY();
+}
+
+//-------------------------
+//Animation control
+//-------------------------
+
+Uint16 SpriteSheet::SetIndex(Uint16) {
+	//
+}
+
+Uint16 SpriteSheet::SetFrame(Uint16) {
+	//
+}
+
+Uint16 SpriteSheet::SetDelay(Uint16) {
+	//
+}
+
+Uint16 SpriteSheet::SetIndex() {
+	//
+}
+
+Uint16 SpriteSheet::SetFrame() {
+	//
+}
+
+Uint16 SpriteSheet::SetDelay() {
+	//
+}
+
+bool SpriteSheet::SetActive(bool) {
+	//
+}
+
+bool SpriteSheet::GetActive() {
+	//
+}
--- Pickin Sticks/sprite_sheet.h
+++ Pickin Sticks/sprite_sheet.h
@@ -4,21 +4,40 @@
 #include "SDL.h"
 
 #include "Codebase/image.h"
+#include "Codebase/animator.h"
 
 class SpriteSheet : public Image {
 public:
 	/* Public access members */
 	SpriteSheet();
-	~SpriteSheet();
+	virtual ~SpriteSheet();
+
+	void Update();
 
 	void LoadSurface(const char* fname, Uint16 width = 0, Uint16 height = 0);
 	void SetSurface(SDL_Surface* surface, Uint16 width = 0, Uint16 height = 0);
 
 	virtual void DrawTo(SDL_Surface* const, Sint16 x, Sint16 y);
 
+	Uint16 SetWidth(Uint16);
+	Uint16 SetHeight(Uint16);
+	Uint16 GetWidth();
+	Uint16 GetHeight();
+
+	/* Animation control */
+	Uint16 SetIndex(Uint16);
+	Uint16 SetFrame(Uint16);
+	Uint16 SetDelay(Uint16);
+	Uint16 SetIndex();
+	Uint16 SetFrame();
+	Uint16 SetDelay();
+
+	bool SetActive(bool);
+	bool GetActive();
+
 private:
 	/* Private access members */
-	//...
+	Animator animator;
 
 	/* Disallow these members */
 	Image::LoadSurface;
@@ -34,8 +53,8 @@ private:
 
 	Image::GetClipX;
 	Image::GetClipY;
-	Image::GetClipW;
-	Image::GetClipH;
+//	Image::GetClipW;
+//	Image::GetClipH;
 };
 
 #endif
